d3ae954d0ab8c7fcbf0020e76218d43e76531055
Manel <mserraro244@alumnes.ub.edu>
2024-06-09 16:23:00 +0200

    Added Python notebook and norm.py

---
+++ norm.py
@@ -0,0 +1,14 @@
+import os 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# set the directory as the current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir('data')
+
+
+
+data = np.loadtxt('./output.txt', skiprows=1)
+
+plt.plot(data[:,0],data[:,1])
+plt.show()
