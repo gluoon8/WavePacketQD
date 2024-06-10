7cd90e3f6c461dda9e7fc3072f877cfe5665c5b2
Manel <mserraro244@alumnes.ub.edu>
2024-06-10 02:15:27 +0200

    some good results

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from numba import njit
+import imageio.v2 as imageio
 
 def real_psi(A, x0, sigma_x, k0, w0, x, t):
     return A*np.exp(-(x-x0)**2/(2*sigma_x**2))*np.cos(k0*x-w0*t)
@@ -15,7 +16,32 @@ def dens_prob(A, x0, sigma_x, x):
 def norm(psiI,psiR):
     return np.sum(abs(psiI)**2 + abs(psiR)**2) 
 
-def initial_conditions(A, x0, sigma_x, k0, w0, L, dt, tfinal, dx):
+def get_potential(potential, x, Xsteps, w0):
+    if potential == 'free':
+        V = np.zeros(Xsteps+1)
+        pot = 0
+
+    elif potential == 'barrier':
+        V = np.zeros(Xsteps+1)
+        V[int(Xsteps/2):int(2*Xsteps/3)] = 0.15  # Barrier in the middle of the grid.
+
+        pot = 1
+
+    elif potential == 'harmonic':
+        m=1
+        V = 0.5*m*w0**2*x**2
+        pot = 2
+
+    elif potential == 'well':
+        V = np.zeros(Xsteps)
+        V[int(Xsteps/2):int(Xsteps/2)+10] = 1e6
+        pot = 3
+    else:
+        raise ValueError('Potential not defined')
+
+    return V, pot
+
+def initial_conditions(A, x0, sigma_x, k0, w0, L, dt, tfinal, dx, potential):
     '''Initial conditions for the wave packet.
     
     - input:
@@ -29,12 +55,14 @@ def initial_conditions(A, x0, sigma_x, k0, w0, L, dt, tfinal, dx):
     - output:
         x: array, spatial grid.
         psiR: array, real part of the wave packet at t = 0.
-        psiI: array, imaginary part of the wave packet a t t = 0.
+        psiI: array, imaginary part of the wave packet at t = 0.
         psiR_0: array, real part of the wave packet at t = 0.
         psiI_0: array, imaginary part of the wave packet at t = 0.
 
     
     '''
+
+
 
     Xsteps = int(L/dx) + 1
 
@@ -60,10 +88,14 @@ def initial_conditions(A, x0, sigma_x, k0, w0, L, dt, tfinal, dx):
     plt.ylabel('psi')
     plt.legend()
     plt.savefig('wavepacketinitial.png')
+    plt.close()
 
-    return x, psiR, psiI, psiR_0, psiI_0
+    V, pot = get_potential(potential, x, Xsteps, w0)
+
+
+    return x, psiR, psiI, psiR_0, psiI_0, V, pot
     
-def welcome(integrate, dt, dx, L, A, x0, sigma_x, k0, w0, tfinal):
+def welcome(integrate, dt, dx, L, A, x0, sigma_x, k0, w0, tfinal, potential):
     print('----------------------')
     print('')
     print('WAVE PACKET SIMULATION')
@@ -75,6 +107,7 @@ def welcome(integrate, dt, dx, L, A, x0, sigma_x, k0, w0, tfinal):
     print('----------PARAMETERS----------')
     print('')
     print(f'integration method: {integrate}')
+    print(f'potential: {potential}')
     print(f'dt:     {dt} s')
     print(f'dx:     {dx} m')
     print(f'L:      {L} m')
@@ -87,10 +120,77 @@ def welcome(integrate, dt, dx, L, A, x0, sigma_x, k0, w0, tfinal):
     print(f'')
     print('-----------------------------')
 
+@njit
+def compute_k(psiR, psiI, psiR_aux, psiI_aux, dt, dx, L, V):
+    h_bar = 1
+    m = 1
+    Xsteps = int(L/dx)
+    factor = dt*h_bar/(2*m * dx**2)
+
+
+    k1r = np.zeros(len(psiR))
+    k1i = np.zeros(len(psiI))
+
+    # print('k1r', k1r.shape)
+    # print('k1i', k1i.shape)
+# 
+    # print('psiI_aux', psiI_aux.shape)
+    # print('psiR_aux', psiR_aux.shape)
+# 
+    # print('psiI', psiI.shape)
+    # print('psiR', psiR.shape)
+    # print('V', V.shape)
+
+
+
+    for j in range(1,Xsteps-1):
+
+        k1r[j] = -  factor * (psiI_aux[j+1] - 2*psiI_aux[j] + psiI_aux[j-1]) + V[j]*psiI_aux[j] / h_bar
+        k1i[j] =    factor * (psiR_aux[j+1] - 2*psiR_aux[j] + psiR_aux[j-1]) - V[j]*psiR_aux[j] / h_bar
+
+    psiI_aux = np.copy(psiI + 0.5*dt*k1i)
+    psiR_aux = np.copy(psiR + 0.5*dt*k1r)
+
+    k2r = np.zeros(len(psiR), dtype=np.float64)
+    k2i = np.zeros(len(psiI), dtype=np.float64)
+
+    for j in range(1,Xsteps-1):
+
+        k2r[j] = -  factor * (psiI_aux[j+1] - 2*psiI_aux[j] + psiI_aux[j-1]) + V[j]*psiI_aux[j] / h_bar
+        k2i[j] =    factor * (psiR_aux[j+1] - 2*psiR_aux[j] + psiR_aux[j-1]) - V[j]*psiR_aux[j] / h_bar
+
+
+    psi2i = psiI + 0.5*dt*k2i
+    psi2r = psiR + 0.5*dt*k2r
+
+    psiI_aux = np.copy(psi2i)
+    psiR_aux = np.copy(psi2r)
+
+    k3r = np.zeros(len(psiR), dtype=np.float64)
+    k3i = np.zeros(len(psiI), dtype=np.float64)
+
+    for j in range(1,Xsteps-1):
+        
+        k3r[j] = -  factor * (psiI_aux[j+1] - 2*psiI_aux[j] + psiI_aux[j-1]) + V[j]*psiI_aux[j] / h_bar
+        k3i[j] =    factor * (psiR_aux[j+1] - 2*psiR_aux[j] + psiR_aux[j-1]) - V[j]*psiR_aux[j] / h_bar
+
+    psiI_aux = np.copy(psiI + dt*k3i)
+    psiR_aux = np.copy(psiR + dt*k3r)
+
+    k4r = np.zeros(len(psiR), dtype=np.float64)
+    k4i = np.zeros(len(psiI), dtype=np.float64)
+
+    for j in range(1,Xsteps-1):
+        
+        k4r[j] = -  factor * (psiI_aux[j+1] - 2*psiI_aux[j] + psiI_aux[j-1]) + V[j]*psiI_aux[j] / h_bar
+        k4i[j] =    factor * (psiR_aux[j+1] - 2*psiR_aux[j] + psiR_aux[j-1]) - V[j]*psiR_aux[j] / h_bar
+    
+
+    return k1r, k2r, k3r, k4r, k1i, k2i, k3i, k4i
 
 
 @njit
-def integrator(psiR, psiR_aux, psiI, psiI_aux, dt, dx, L, integrate):
+def integrator(psiR, psiR_aux, psiI, psiI_aux, dt, dx, L, integrate, V):
     h_bar = 1
     m = 1
     Xsteps = int(L/dx)    
@@ -102,14 +202,23 @@ def integrator(psiR, psiR_aux, psiI, psiI_aux, dt, dx, L, integrate):
     if integrate == 'euler':
         for j in range(1,Xsteps-1):
 
-            psiR[j] = psiR_aux[j] - factor*(psiI_aux[j+1]-2*psiI_aux[j]+psiI_aux[j-1]) 
-            psiI[j] = psiI_aux[j] + factor*(psiR_aux[j+1]-2*psiR_aux[j]+psiR_aux[j-1]) 
+            psiR[j] = psiR_aux[j] - factor*(psiI_aux[j+1]-2*psiI_aux[j]+psiI_aux[j-1]) + V[j]*psiI_aux[j]*dt
+            psiI[j] = psiI_aux[j] + factor*(psiR_aux[j+1]-2*psiR_aux[j]+psiR_aux[j-1]) - V[j]*psiR_aux[j]*dt
+
+    elif integrate == 'rk4':
+        k1r, k2r, k3r, k4r, k1i, k2i, k3i, k4i = compute_k(psiR, psiI, psiR_aux, psiI_aux, dt, dx, L, V)
+        psiR = psiR + dt/6. * (k1r + 2*k2r + 2*k3r + k4r)
+        psiI = psiI + dt/6. * (k1i + 2*k2i + 2*k3i + k4i)
+       
 
     return psiR, psiI, psiR_aux, psiI_aux
 
 
 
-def plot_wave_packet(x, psiR, psiI, i, dt):
+
+
+
+def plot_wave_packet(x, psiR, psiI, i, dt,pot, V):
     '''
     Plot the wave packet at time i*dt.
 
@@ -118,6 +227,8 @@ def plot_wave_packet(x, psiR, psiI, i, dt):
     plt.figure()
     plt.plot(x, psiR, label='psi_Re')
     plt.plot(x, psiI, label='psi_Im')
+    if pot == 1:
+        plt.plot(x, V, label='Barrier')
     plt.legend()
     plt.title(f'Time: {i*dt:2f} s')
     plt.ylim(-0.3, 0.3)
--- wavepack.py
+++ wavepack.py
@@ -1,14 +1,15 @@
 from functions import *
 import os
 #import imageio
-import imageio.v2 as imageio
+
 
 # Set the working directory to the current folder
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Equation of motion of real part and imaginary part
-integrate = 'euler'
+integrate = 'rk4'
+potential = 'free'
 dt = 1e-5
 dx = 0.1
 m = 1
@@ -17,8 +18,8 @@ L = 100
 sigma_x = 5
 w0 = 2
 k0 = 3
-x0 = 25
-tfinal = 25
+x0 = 20
+tfinal = 2
 
 A = 1 / (2*np.pi*sigma_x**2)**0.25
 
@@ -27,11 +28,11 @@ Tsteps = int(tfinal/dt)
 Xsteps = int(L/dx) + 1
 
 
-welcome(integrate, dt, dx, L, A, x0, sigma_x, k0, w0, tfinal)
+welcome(integrate, dt, dx, L, A, x0, sigma_x, k0, w0, tfinal, potential)
 
 #----------------Inicialització dels paquets d'ones----------------
 
-x, psiR, psiI, psiR_0, psiI_0 = initial_conditions(A, x0, sigma_x, k0, w0, L, dt, tfinal, dx)
+x, psiR, psiI, psiR_0, psiI_0, V, pot = initial_conditions(A, x0, sigma_x, k0, w0, L, dt, tfinal, dx, potential)
 
 
 psiR_aux = np.copy(psiR)
@@ -43,16 +44,16 @@ image_files = []
 
 # Go to pics directory
 os.chdir('out')
-
+ 
 
 for i in range(0,Tsteps):
 
 
     if i % 10000 == 0:
-        filename = plot_wave_packet(x, psiR, psiI, i, dt)
+        filename = plot_wave_packet(x, psiR, psiI, i, dt, pot, V)
         image_files.append(filename)
-
-    psiR, psiI, psiR_aux, psiI_aux = integrator(psiR, psiR_aux, psiI, psiI_aux, dt, dx, L, integrate)
+    print('Time step: ', i)
+    psiR, psiI, psiR_aux, psiI_aux = integrator(psiR, psiR_aux, psiI, psiI_aux, dt, dx, L, integrate, V)
     
     norma.append(norm(psiI, psiR))
 
